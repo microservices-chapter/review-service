209e734748fa4341cea76897a2a7fd1ed77d9728
Ben Edgar <ben.edgar@appian.com>
2018-02-23 14:44:52 -0500

    Updated review app, need to get add review working

--- src/main/java/com/appian/microservices/review/Review.java
+++ src/main/java/com/appian/microservices/review/Review.java
@@ -2,8 +2,15 @@ package com.appian.microservices.review;
 
 public class Review {
   private String customerId;
+
   private String reviewHeader;
   private String reviewBody;
+
+  public Review() {
+    this.customerId = "";
+    this.reviewHeader = "";
+    this.reviewBody = "";
+  }
 
   public Review(String customerId, String reviewHeader, String reviewBody) {
     this.customerId = customerId;
@@ -21,5 +28,17 @@ public class Review {
 
   public String getReviewBody() {
     return reviewBody;
+  }
+
+  public void setCustomerId(String customerId) {
+    this.customerId = customerId;
+  }
+
+  public void setReviewHeader(String reviewHeader) {
+    this.reviewHeader = reviewHeader;
+  }
+
+  public void setReviewBody(String reviewBody) {
+    this.reviewBody = reviewBody;
   }
 }
